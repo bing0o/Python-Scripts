3c3303953cce4137a6acd7db48749d07b8be227e
med pro <25457545+bing0o@users.noreply.github.com>
2019-11-09 11:26:19 +0100

    Update crypto.py; new version

--- crypto.py
+++ crypto.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python
 
-import os, time
+import os, time, argparse
+# sudo pip install pycrypto
 from Crypto.Cipher import AES
 from Crypto.Hash import SHA256
 from Crypto import Random
-from optparse import *
-
-class colors:
-        def __init__(self):
-                self.blue = "\033[94m"
-                self.red = "\033[91m"
-                self.end = "\033[0m"
-cl = colors()
 
 
+green = "\033[32m"
+blue = "\033[34m"
+red = "\033[31m"
+bold = "\033[1m"
+end = "\033[0m"
 
-print(cl.red+"""
+print(red+"""
 
 \t
 \t               |
@@ -33,36 +31,18 @@ print(cl.red+"""
 \t          \|    |      |      |      |    /
 \t           \____|______|______|______|___/
 
+\t               By: @hack1lab, @bing0o
+\t            fb.me/hack1lab, fb.me/pOOn3r
 
-
-\t              By: @hacklab, @mohamed1lar
-\t          fb.me/hack1lab, fb.me/mohamed1lar
-
-"""+cl.end)
-
-
-parser = OptionParser("""
-
-#Usage:
-
-	-e   to encrypt files
-	-d   to decrypt files
-	-p   password to encryption or decryption files
-
-#Example:
-
-	python crypto.py -e <Your File> -p <Password>
-
-	python crypto.py -d <Your File> -p <Password>
-
-
-""")
+"""+end)
 
 def encrypt(key, filename):
 	chunksize = 64*1024
 	outputFile = filename+".hacklab"
-	filesize = str(os.path.getsize(filename)).zfill(16)
+	size = os.path.getsize(filename) #+ 16
+	filesize = str(size).zfill(16)
 	IV = Random.new().read(16)
+	secret = "0000hack1lab0000"
 
 	encryptor = AES.new(key, AES.MODE_CBC, IV)
 
@@ -70,6 +50,7 @@ def encrypt(key, filename):
 		with open(outputFile, 'wb') as outfile:
 			outfile.write(filesize.encode('utf-8'))
 			outfile.write(IV)
+			outfile.write(encryptor.encrypt(secret))
 
 			while True:
 				chunk = infile.read(chunksize)
@@ -81,9 +62,11 @@ def encrypt(key, filename):
 
 				outfile.write(encryptor.encrypt(chunk))
 
+
 def decrypt(key, filename):
 	chunksize = 64 * 1024
 	outputFile = filename.split('.hacklab')[0]
+
 
 	with open(filename, 'rb') as infile:
 		filesize = int(infile.read(16))
@@ -91,47 +74,81 @@ def decrypt(key, filename):
 		decryptor = AES.new(key, AES.MODE_CBC, IV)
 
 		with open(outputFile, 'wb') as outfile:
+
 			while True:
 				chunk = infile.read(chunksize)
 
 				if len(chunk) == 0:
 					break
 
-				outfile.write(decryptor.decrypt(chunk))
+				chunk = str(decryptor.decrypt(chunk))
+				chunk = chunk.replace("0000hack1lab0000", "")
+				outfile.write(chunk)
 			outfile.truncate(filesize)
+
+
+def check(key, filename):
+	chunksize = 64 * 1024
+	secret = "0000hack1lab0000"
+	
+	with open(filename, 'rb') as infile:
+		IV = infile.read(16)
+		decryptor = AES.new(key, AES.MODE_CBC, IV)
+
+		chunk = infile.read(chunksize)
+		test = decryptor.decrypt(chunk)
+		if secret not in test:
+			print(red+bold+"[!] Wrong Password!"+end)
+			exit(1)
+
+
+
 
 def getkey(password):
 	hasher = SHA256.new(password.encode('utf-8'))
 	return hasher.digest()
 
-def Main():
-	parser.add_option("-e",dest="en",type="string", help="This Option To Encryption Files")
-	parser.add_option("-d",dest="de",type="string", help="This Option To Decrytpion Files")
-	parser.add_option("-p",dest="password",type="string", help="Enter Password")
-	(options, args) = parser.parse_args()
-	if options.en == None and options.de == None:
-		print(cl.blue+parser.usage+cl.end)
-		exit(0)
-	else:
-		en = str(options.en)
-		de = str(options.de)
-		password = str(options.password)
-		if options.de == None:
-			print(cl.red+"[+] Encrypting......"+cl.end)
-			encrypt(getkey(password), en)
-			print(cl.red+"[+] removing file......"+cl.end)
-			time.sleep(1.5)
-			os.remove(en)
-			print(cl.red+"\n[+] Done"+cl.end)
-		elif options.en == None:
-			print(cl.red+"[+] Decrypting......"+cl.end)
-			decrypt(getkey(password), de)
-			print(cl.red+"[+] removing file......"+cl.end)
-			os.remove(de)
-			time.sleep(1.5)
-			print(cl.red+'\n[+] Done'+cl.end)
-		else:
-			print("Oops operation failed")
+def main():
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-e", "--crypt", help="File Encryption", type=str)
+	parser.add_argument("-d", "--dcrypt", help="File Decryption", type=str)
+	parser.add_argument("-p", "--password", help="Password To Encrypt/Decrypt a File", type=str)
+	parser.add_argument("-x", "--delete", help="Delete The Original File", action='store_true')
+	args = parser.parse_args()
+
+	enc = str(args.crypt)
+	dec = str(args.dcrypt)
+	password = str(args.password)
+	dd = str(args.delete)
+
+	if enc == "None" and dec == "None":
+		parser.print_help()
+		exit(1)
+
+	if password == "None":
+		password = raw_input("[+] Enter Password: ")
+
+	if enc != "None":
+		print(blue+"[+] Encrypt: "+end+"[ "+enc+" ]")
+		encrypt(getkey(password), enc)
+		print(blue+"[+] Output: "+end+"[ "+enc+".hacklab"+" ]")
+		if dd == "True":
+			print(red+"[!] Remove: "+end+"[ "+enc+" ]")
+			os.remove(enc)
+		print(green+bold+"[*] Done!"+end)
+
+	elif dec != "None":
+		print(blue+"[+] Decrypt: "+end+"[ "+dec+" ]")
+		check(getkey(password), dec)
+		decrypt(getkey(password), dec)
+		name = dec.split(".hacklab")[0]
+		print(blue+"[+] Output: "+end+"[ "+name+" ]")
+		if dd == "True":
+			print(red+"[!] Remove: "+end+"[ "+dec+" ]")
+			os.remove(dec)
+		print(green+bold+"[*] Done!"+end)
+
+
 
 if __name__ == '__main__':
-	Main()
+	main()
